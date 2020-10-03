459e31eef423ab6ab1ceb0d588b807bea9494353
d-hayashi <hayashi.daiki@nagoya-u.jp>
2020-10-03 16:26:36 +0900

    Export `HealthCheck` component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import { fetchApi, FetchStatus } from './api/ApiUtils'
 import { AUTH_CONFIG } from './auth/config'
 import { APP_CATALOG, API_CATALOG } from './api/ApiConstant'
+import HealthCheck from './components/HealthCheck'
 
 export { fetchApi, FetchStatus }
 export { AUTH_CONFIG }
 export { APP_CATALOG, API_CATALOG }
+export { HealthCheck }
