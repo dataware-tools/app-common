63d4caabcc7381a41f185029a992a31fde1ad0d7
d-hayashi <hayashi.daiki@nagoya-u.jp>
2020-10-03 17:17:31 +0900

    Enrich the content of catalogs

--- src/api/ApiConstant.tsx
+++ src/api/ApiConstant.tsx
@@ -1,9 +1,29 @@
 export const API_CATALOG = {
-  authTest: '/auth_test'
+  authTest: {
+    id: 'auth_test',
+    name: 'Test authentication',
+    description: 'Check if authentication passes',
+    endpoint: '/auth_test'
+  }
 }
 
 export const APP_CATALOG = {
-  webviz: 'http://webviz.hdwlab.co.jp/',
-  docs: '/docs',
-  dataBrowser: '/data_browser'
+  webviz: {
+    id: 'webviz',
+    name: 'WebViz',
+    description: 'Modern rosbag player',
+    urlPrefix: 'http://webviz.hdwlab.co.jp/'
+  },
+  docs: {
+    id: 'data_docs',
+    name: 'Documents',
+    description: 'Read documents',
+    urlPrefix: '/docs'
+  },
+  dataBrowser: {
+    id: 'data_browser',
+    name: 'Data browser',
+    description: 'Explore data',
+    urlPrefix: '/data_browser',
+  }
 }
