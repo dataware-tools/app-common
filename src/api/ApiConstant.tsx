94a811489bdb8ecbbd792c04b4e9b56ff889ae35
d-hayashi <hayashi.daiki@nagoya-u.jp>
2020-10-03 17:19:59 +0900

    Add icons

--- src/api/ApiConstant.tsx
+++ src/api/ApiConstant.tsx
@@ -10,18 +10,21 @@ export const API_CATALOG = {
 export const APP_CATALOG = {
   webviz: {
     id: 'webviz',
+    icon: 'https://webviz.io/favicon.ico',
     name: 'WebViz',
     description: 'Modern rosbag player',
     urlPrefix: 'http://webviz.hdwlab.co.jp/'
   },
   docs: {
     id: 'data_docs',
+    icon: 'book',
     name: 'Documents',
     description: 'Read documents',
     urlPrefix: '/docs'
   },
   dataBrowser: {
     id: 'data_browser',
+    icon: 'database',
     name: 'Data browser',
     description: 'Explore data',
     urlPrefix: '/data_browser',
