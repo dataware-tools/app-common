63797dfdaebe8edd119a85e85fc4f1eb3aaf9652
d-hayashi <hayashi.daiki@nagoya-u.jp>
2020-10-04 11:34:22 +0900

    Add utils for posting data

--- src/api/utils.tsx
+++ src/api/utils.tsx
@@ -3,7 +3,7 @@ import { Loader, Message } from 'semantic-ui-react'
 import request from 'superagent'
 
 export const fetchApi = (
-  q: string,
+  endpoint: string,
   accessToken: string | undefined = undefined,
   setState = (..._: any) => {},
   callback = (_: any | undefined) => {},
@@ -16,7 +16,7 @@ export const fetchApi = (
     isFetching: true
   })
   request
-    .get(q)
+    .get(endpoint)
     .set('Authorization', 'Bearer ' + accessToken || '')
     .query(query)
     .then((res: any) => {
@@ -65,6 +65,80 @@ export const FetchStatus = (props: FetchStatusProps) => {
     <div>
       {props.isFetching && <Loader inline active size='mini' />}
       {props.isFetchDone && props.isFetchFailed && (
+        <Message
+          negative
+          icon='exclamation triangle'
+          header='Fetch failed'
+          content={getContent()}
+        />
+      )}
+    </div>
+  )
+}
+
+export const postApi = (
+  endpoint: string,
+  accessToken: string | undefined = undefined,
+  setState = (..._: any) => {},
+  callback = (_: any | undefined) => {},
+  data: object = { undefined }
+) => {
+  setState({
+    res: undefined,
+    isPostFailed: false,
+    isPostDone: false,
+    isPosting: true
+  })
+  request
+    .get(endpoint)
+    .set('Authorization', 'Bearer ' + accessToken || '')
+    .send(data)
+    .then((res: any) => {
+      setState(
+        {
+          res: res.body,
+          isPostFailed: false,
+          isPostDone: true,
+          isPosting: false
+        },
+        callback(res)
+      )
+    })
+    .catch((err: any) => {
+      console.error(err)
+      setState(
+        {
+          res:
+            err && err.response && err.response.body ? err.response.body : {},
+          isPostFailed: true,
+          isPostDone: true,
+          isPosting: false
+        },
+        callback(undefined)
+      )
+    })
+}
+
+type PostStatusProps = {
+  res?: any
+  isPostDone?: boolean
+  isPostFailed?: boolean
+  isPosting?: boolean
+}
+
+export const PostStatus = (props: PostStatusProps) => {
+  const getContent = (): string => {
+    if (props.res && props.res.error) {
+      return 'Error: ' + props.res.error
+    } else {
+      return 'Could not send data.'
+    }
+  }
+
+  return (
+    <div>
+      {props.isPosting && <Loader inline active size='mini' />}
+      {props.isPostDone && props.isPostFailed && (
         <Message
           negative
           icon='exclamation triangle'
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { fetchApi, FetchStatus } from './api/utils'
+import { fetchApi, FetchStatus, postApi, PostStatus } from './api/utils'
 import { AUTH_CONFIG } from './auth/config'
 import { APP_CATALOG } from './app/catalog'
 import { API_CATALOG } from './api/catalog'
@@ -7,6 +7,8 @@ import HealthCheck from './components/HealthCheck'
 export {
   fetchApi,
   FetchStatus,
+  postApi,
+  PostStatus,
   AUTH_CONFIG,
   APP_CATALOG,
   API_CATALOG,
