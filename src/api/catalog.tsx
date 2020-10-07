dc175958870f53c7811415932b0ed62e1c611e1c
d-hayashi <hayashi.daiki@nagoya-u.jp>
2020-10-07 18:30:19 +0900

    Move catalogs to `dataware-tools/catalog`

--- src/api/catalog.tsx
+++ src/api/catalog.tsx
@@ -1,20 +1,25 @@
-export const API_CATALOG = {
-  authTest: {
-    id: 'auth_test',
-    name: 'Test authentication',
-    description: 'Check if authentication passes',
-    endpoint: '/auth_test'
-  },
-  databaseStore: {
-    id: 'database_store',
-    name: 'Database store',
-    description: 'An API for storing meta-information of databases',
-    endpoint: '/database_store'
-  },
-  recordStore: {
-    id: 'record_store',
-    name: 'Record store',
-    description: 'An API for fetching record metadata',
-    endpoint: '/record_store'
+class APICatalog {
+  public _catalogUrl: string
+  constructor() {
+    this._catalogUrl =
+      process.env.REACT_APP_COMMON_API_CATALOG ||
+      'https://raw.githubusercontent.com/dataware-tools/catalog/master/api.json'
+
+    this.loadCatalog()
+  }
+
+  private loadCatalog() {
+    fetch(this._catalogUrl)
+      .then((res) => res.json())
+      .then((out) => {
+        Object.keys(out).forEach((key: string) => {
+          this[key] = out[key]
+        }, this)
+      })
+      .catch((err) => {
+        throw err
+      })
   }
 }
+
+export const API_CATALOG = new APICatalog()
--- src/app/catalog.tsx
+++ src/app/catalog.tsx
@@ -1,29 +1,25 @@
-export const APP_CATALOG = {
-  webviz: {
-    id: 'webviz',
-    icon: 'https://webviz.io/favicon.ico',
-    name: 'WebViz',
-    description: 'Modern rosbag player',
-    urlPrefix: 'http://webviz.hdwlab.co.jp/',
-    visibility: 'public',
-    location: 'external'
-  },
-  docs: {
-    id: 'data_docs',
-    icon: 'book',
-    name: 'Documents',
-    description: 'Read documents',
-    urlPrefix: 'http://192.168.1.122:32332',
-    visibility: 'private',
-    location: 'internal'
-  },
-  dataBrowser: {
-    id: 'data_browser',
-    icon: 'database',
-    name: 'Data browser',
-    description: 'Explore data',
-    urlPrefix: '/data-browser',
-    visibility: 'private',
-    location: 'internal'
+class AppCatalog {
+  public _catalogUrl: string
+  constructor() {
+    this._catalogUrl =
+      process.env.REACT_APP_COMMON_APP_CATALOG ||
+      'https://raw.githubusercontent.com/dataware-tools/catalog/master/app.json'
+
+    this.loadCatalog()
+  }
+
+  private loadCatalog() {
+    fetch(this._catalogUrl)
+      .then((res) => res.json())
+      .then((out) => {
+        Object.keys(out).forEach((key: string) => {
+          this[key] = out[key]
+        }, this)
+      })
+      .catch((err) => {
+        throw err
+      })
   }
 }
+
+export const APP_CATALOG = new AppCatalog()
