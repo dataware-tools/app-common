a71a408fa5d57361f0c424a45dc8709e68921162
d-hayashi <hayashi.daiki@nagoya-u.jp>
2020-11-04 14:37:21 +0900

    Change the visibility of 'catalog' to private

--- bin/download_catalog.sh
+++ bin/download_catalog.sh
@@ -6,17 +6,25 @@
 set -e
 
 function getCatalog() {
+  set -e
   URL=$1
   if [[ ${URL} =~ http.* ]]; then
-    curl ${URL}
+    [[ -z "${GITHUB_TOKEN}" ]] && echo "env GITHUB_TOKEN must be set" >&2 && exit 1
+    curl -H "Authorization: token ${GITHUB_TOKEN}" "${URL}"
+  elif [[ ${URL} =~ git@.* ]]; then
+    workdir=$(mktemp -d)
+    git clone --depth 1 "${URL%.git*}" "${workdir}/catalog" > /dev/null
+    cat "${workdir}/catalog/${URL##*.git}"
   else
-    cat ${URL}
+    cat "${URL}"
   fi
 }
 
 # Default parameters
-DEFAULT_API_CATALOG=https://raw.githubusercontent.com/dataware-tools/catalog/master/api.json
-DEFAULT_APP_CATALOG=https://raw.githubusercontent.com/dataware-tools/catalog/master/app.json
+#DEFAULT_API_CATALOG=https://raw.githubusercontent.com/dataware-tools/catalog/master/api.json
+#DEFAULT_APP_CATALOG=https://raw.githubusercontent.com/dataware-tools/catalog/master/app.json
+DEFAULT_API_CATALOG=git@github.com:dataware-tools/catalog.git/api.json
+DEFAULT_APP_CATALOG=git@github.com:dataware-tools/catalog.git/app.json
 
 # Set the default parameters if not specified
 [[ -z "${API_CATALOG}" ]] && echo "Setting API_CATALOG to ${DEFAULT_API_CATALOG}" && API_CATALOG=${DEFAULT_API_CATALOG}
