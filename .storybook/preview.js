4614a908e0b757357d08b815e68e4c8d9ce707e1
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-04-26 10:55:05 +0900

    add story book

---
+++ .storybook/preview.js
@@ -0,0 +1,45 @@
+import { AUTH_CONFIG } from '../src/auth/config'
+
+import { Auth0Provider } from '@auth0/auth0-react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import React from 'react'
+import { ThemeProvider, StylesProvider } from '@material-ui/core/styles'
+import theme from '../src/theme'
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' }
+}
+const redirectUri = window.location.origin
+
+const onRedirectCallback = (appState) => {
+  const nonQueryParamURL =
+    appState && appState.returnTo ? appState.returnTo : window.location.origin
+  history.replaceState(null, '', nonQueryParamURL)
+}
+export const decorators = [
+  (story) => {
+    return (
+      <>
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Oxanium:wght@300;400;500;700&display=swap'
+          rel='stylesheet'
+        />
+        <StylesProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Auth0Provider
+              domain={AUTH_CONFIG.domain}
+              clientId={AUTH_CONFIG.clientId}
+              audience={AUTH_CONFIG.apiUrl}
+              redirectUri={redirectUri}
+              onRedirectCallback={onRedirectCallback}
+            >
+              {story()}
+            </Auth0Provider>
+          </ThemeProvider>
+        </StylesProvider>
+      </>
+    )
+  }
+]
