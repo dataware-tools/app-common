235ab0bad6c0b1cd7e86d5bc073b28205bd07931
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-05-10 21:22:59 +0900

    add LoadingIndicator

---
+++ src/components/LoadingIndicator.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+type Props = { classes: ReturnType<typeof useStyles> }
+
+const Component = ({ classes }: Props): JSX.Element => {
+  return (
+    <div className={classes.root}>
+      <CircularProgress />
+    </div>
+  )
+}
+
+const useStyles = makeStyles(() => ({
+  root: {
+    alignItems: 'center',
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'center',
+    overflow: 'auto',
+    width: '100%'
+  }
+}))
+
+const Container = (): JSX.Element => {
+  const classes = useStyles()
+  return <Component classes={classes} />
+}
+
+export { Container as LoadingIndicator }
