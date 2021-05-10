67ec5674ee62424a978f58aec367b80b7207dd05
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-05-10 21:30:20 +0900

    add PageWrapper

---
+++ src/components/PageWrapper.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Story } from '@storybook/react'
+import { PageWrapper, PageWrapperProps } from './PageWrapper'
+
+export default {
+  component: PageWrapper,
+  title: 'PageWrapper'
+}
+
+const Template: Story<PageWrapperProps> = (args) => <PageWrapper {...args} />
+export const Default = Template.bind({})
+Default.args = {
+  children: 'this is test',
+  repository: 'https://github.com/dataware-tools/app-common'
+}
