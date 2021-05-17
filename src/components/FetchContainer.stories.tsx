6e73a8b8430eb96b4d708a12e7b2b82c4d68e5f5
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-05-17 14:08:51 +0900

    add FetchContainer

---
+++ src/components/FetchContainer.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { FetchContainer, FetchContainerProps } from './FetchContainer'
+import { Story } from '@storybook/react'
+
+export default {
+  component: FetchContainer,
+  title: 'FetchContainer'
+}
+
+const Template: Story<FetchContainerProps> = (args) => (
+  <FetchContainer {...args}>success!</FetchContainer>
+)
+
+export const Error = Template.bind({})
+Error.args = {
+  isFetchSuccess: false,
+  isFetchEnd: true,
+  errorMessage: { reason: 'test', instruction: 'test' }
+}
+
+export const Waiting = Template.bind({})
+Waiting.args = {
+  isFetchSuccess: true,
+  isFetchEnd: false,
+  errorMessage: { reason: 'test', instruction: 'test' }
+}
+
+export const Success = Template.bind({})
+Success.args = {
+  isFetchSuccess: true,
+  isFetchEnd: true,
+  errorMessage: { reason: 'test', instruction: 'test' }
+}
