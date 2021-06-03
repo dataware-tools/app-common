264753400dd0e9dd1974a70adc82853d99e04340
d-hayashi <daiki@d-hayashi.me>
2021-06-03 14:20:29 +0900

    Use environment variables to change AUTH_CONFIG

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -1,5 +1,17 @@
 export const AUTH_CONFIG = {
-  domain: 'hdwlab-com.us.auth0.com',
-  clientId: 'xkLwy0jD9bt5pv7sitXT23FXigzuYC3j',
-  apiUrl: 'https://tools.hdwlab.com/'
+  domain: process.env.REACT_APP_DATAWARE_TOOLS_AUTH_CONFIG_DOMAIN
+    ? process.env.REACT_APP_DATAWARE_TOOLS_AUTH_CONFIG_DOMAIN
+    : process.env.NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_DOMAIN
+    ? process.env.NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_DOMAIN
+    : 'dataware-tools.us.auth0.com',
+  clientId: process.env.REACT_APP_DATAWARE_TOOLS_AUTH_CONFIG_CLIENT_ID
+    ? process.env.REACT_APP_DATAWARE_TOOLS_AUTH_CONFIG_CLIENT_ID
+    : process.env.NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_CLIENT_ID
+    ? process.env.NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_CLIENT_ID
+    : 'ETb1RhJEbtXlFgWtaHzl5kPCkaYqhTVl',
+  apiUrl: process.env.REACT_APP_DATAWARE_TOOLS_AUTH_CONFIG_API_URL
+    ? process.env.REACT_APP_DATAWARE_TOOLS_AUTH_CONFIG_API_URL
+    : process.env.NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_API_URL
+    ? process.env.NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_API_URL
+    : 'https://demo.dataware-tools.com/'
 }
