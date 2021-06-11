f66143fbbf717290bf98525d9cd0fe5e56346816
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-06-11 17:18:57 +0900

    add TextCenteringSpan

---
+++ src/components/TextCenteringSpan.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { TextCenteringSpan } from './TextCenteringSpan'
+import Button from '@material-ui/core/Button'
+import AddCircle from '@material-ui/icons/AddCircle'
+
+export default {
+  component: TextCenteringSpan,
+  title: 'TextCenteringSpan'
+}
+
+export const Default = (): JSX.Element => (
+  <>
+    <span>centering</span>
+    <br />
+    <Button startIcon={<AddCircle />}>
+      <TextCenteringSpan>test</TextCenteringSpan>
+    </Button>
+    <br />
+    <span>not centering</span>
+    <br />
+    <Button startIcon={<AddCircle />}>test</Button>
+  </>
+)
