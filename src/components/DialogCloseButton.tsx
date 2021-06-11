3e4e8f276d849d4608fa8fa9f80b44cf75c40637
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-06-11 17:16:49 +0900

    add component for layouting dialog

---
+++ src/components/DialogCloseButton.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ToolBar } from './ToolBar'
+import CloseIcon from '@material-ui/icons/Close'
+import { SquareIconButton } from './SquareIconButton'
+
+type ComponentProps = { onClick: () => void }
+const Component = ({ onClick }: ComponentProps): JSX.Element => (
+  <ToolBar
+    right={
+      <SquareIconButton
+        onClick={onClick}
+        icon={<CloseIcon fontSize='large' />}
+      />
+    }
+  />
+)
+
+export { Component as DialogCloseButton }
+export type { ComponentProps as DialogCloseButtonProps }
