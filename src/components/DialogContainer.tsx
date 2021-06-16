b34c598518e74121807f82efe616e28aa699c24f
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-06-16 10:08:14 +0900

    add maxHeight props

--- src/components/DialogContainer.tsx
+++ src/components/DialogContainer.tsx
@@ -1,19 +1,20 @@
 import React, { ReactNode } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 
-type Props = { children: ReactNode; height?: string }
+type Props = { children: ReactNode; height?: string; maxHeight?: string }
 
 const useStyles = makeStyles({
   div: {
     display: 'flex',
     flexDirection: 'column',
     height: (props: Props) => (props.height ? props.height : '100%'),
+    maxHeight: (props: Props) => (props.maxHeight ? props.maxHeight : '100%'),
     padding: '10px'
   }
 })
 
-const Component = ({ children, height }: Props): JSX.Element => {
-  const classes = useStyles({ children, height })
+const Component = ({ children, ...delegated }: Props): JSX.Element => {
+  const classes = useStyles({ children, ...delegated })
   return <div className={classes.div}>{children}</div>
 }
 
