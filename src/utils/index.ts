7f29b1b702925161c273fb0a52d7ba699084bc98
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-06-24 20:19:16 +0900

    Add util func

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,17 @@
+import { useEffect, useRef } from 'react'
+
+const usePrevious = <T>(value: T): T | undefined => {
+  const ref = useRef<T>()
+  useEffect(() => {
+    ref.current = value
+  })
+  return ref.current
+}
+
+const sleep = (ms: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, ms))
+
+export { usePrevious, sleep }
 export * from './queryStringController'
 export * from './types'
 export * from './window'
