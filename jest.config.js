9f810621df5765f294aef797873cc9a9b2ffa130
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-08-25 18:13:32 +0900

    Fix code format of misc config file

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'ts-jest',
-  moduleDirectories: ['node_modules', 'src'],
-  setupFilesAfterEnv: ['./src/setupTest.ts']
-}
+  preset: "ts-jest",
+  moduleDirectories: ["node_modules", "src"],
+  setupFilesAfterEnv: ["./src/setupTest.ts"],
+};
