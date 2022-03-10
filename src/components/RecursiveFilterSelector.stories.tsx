d0254d6e6c51329b1890d3481f591097b7e16156
Toshimitsu Watanabe <kz86nu@gmail.com>
2022-03-10 15:54:54 +0900

    Fix story to ease testing that "position: stircy" work well

--- src/components/RecursiveFilterSelector.stories.tsx
+++ src/components/RecursiveFilterSelector.stories.tsx
@@ -31,8 +31,16 @@ const defaultArgs: RecursiveFilterSelectorProps = {
         {
           label: "group-1-1",
           children: [
-            { label: "Test", key: "group-1-1-test", number: 100 },
+            { label: "Test0", key: "group-1-1-test0", number: 100 },
+            { label: "Test1", key: "group-1-1-test1", number: 100 },
             { label: "Test2", key: "group-1-1-test2", number: 100 },
+            { label: "Test3", key: "group-1-1-test3", number: 100 },
+            { label: "Test4", key: "group-1-1-test4", number: 100 },
+            { label: "Test5", key: "group-1-1-test5", number: 100 },
+            { label: "Test6", key: "group-1-1-test6", number: 100 },
+            { label: "Test7", key: "group-1-1-test7", number: 100 },
+            { label: "Test8", key: "group-1-1-test8", number: 100 },
+            { label: "Test9", key: "group-1-1-test9", number: 100 },
           ],
         },
         { label: "Test", key: "group-1-test", number: 100 },
